d002195516367f4cf4f7d59260768b09cf973905
austinwoo123 <austinwoo123@gmail.com>
2021-02-26 11:57:25 -0800

    finish navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Books from "./pages/Books";
+import Nav from "./components/Nav";
 
 
 function App() {
   return (
     <div>
       <Nav />
-      <Books />
+      {/* <Search /> */}
     </div>
   );
 }
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">
+                Google Books
+      </a>
+
+            <li><a href="">Search</a></li>
+            <li><a href="">Saved</a></li>
+
+
+        </nav>
+    );
+}
+
+export default Nav;
