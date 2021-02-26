00508b2299b436641a7adf61426f9dd0968932f3
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-02-26 13:44:28 -0800

    Update package.json and rename book.js to Book.js

--- client/controllers/booksController.js
+++ client/controllers/booksController.js
@@ -4,7 +4,7 @@ const db = require("../models");
 module.exports = {
  // find all
   findAll: function (req, res) {
-    db.book
+    db.Book
       .find()
       .sort("title")
       .then(book => res.status(200).json(book))
@@ -13,21 +13,21 @@ module.exports = {
   
   // create
   create: function(req, res) {
-    db.book
+    db.Book
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   // find by id
   findById: function(req, res) {
-    db.book
+    db.Book
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   // update
   update: function(req, res) {
-    db.book
+    db.Book
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
---
+++ routes/models/book.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, DataTypes) {
+  var Book = sequelize.define("Book", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+ 
+    authors: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+ 
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+ 
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+ 
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+ 
+  });
+  return Post;
+};
