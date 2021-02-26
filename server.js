cdb6d58c6f83f6cf2b5b2fe402c0d92d15793ff3
austinwoo123 <austinwoo123@gmail.com>
2021-02-26 11:09:06 -0800

    initialize connection to mongoDb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
+
+app.use("/api", apiRoutes);
+
 
 // Define API routes here
 
