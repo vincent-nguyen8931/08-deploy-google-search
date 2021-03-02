6e0111ab7aa391c64a8691514cdbc126b115c50f
austinwoo123 <austinwoo123@gmail.com>
2021-03-02 14:06:58 -0800

    edit stayling of saved page

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -16,27 +16,6 @@ function BookSearch() {
 
 
 
-        //         <div
-        //             style={{ height: 300, clear: "both", paddingTop: 80, textAlign: "left" }}
-        //             className="jumbotron"
-        //         >
-        //             <h2>Book Search</h2>
-        //             <h3>Book: </h3>
-        //             <form>
-        //                 <Input
-        //                     onChange={() => { }}
-        //                     name="title"
-        //                     placeholder="Title (required)"
-        //                 />
-
-
-        //                 <FormBtn
-        //                     disabled={!(formObject.author && formObject.title)}
-        //                     onClick={() => { }}
-        //                 >
-        //                     Submit Book
-        //                 </FormBtn>
-        //             </form>
 
 
 
--- client/src/components/SavedBooks/index.js
+++ client/src/components/SavedBooks/index.js
@@ -10,13 +10,24 @@ function SavedBooks() {
                 <div className="col-sm-12">
                     <div className="card">
                         <div className="card-body">
-                            <h3 className="card-title">Results:</h3>
+                            <h3 className="card-title">Saved Books:</h3>
                             <p className="card-text"></p>
                             <a href="#" className="btn btn-primary">View</a>
                             <a href="#" className="btn btn-primary">Delete</a>
 
                         </div>
                     </div >
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="card-title">Saved Books:</h3>
+                            <p className="card-text"></p>
+
+                            <a href="#" className="btn btn-primary">View</a>
+                            <a href="#" className="btn btn-primary">Delete</a>
+
+                        </div>
+
+                    </div>
 
 
                 </div>
