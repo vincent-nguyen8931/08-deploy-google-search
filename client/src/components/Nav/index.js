5103cab499942c82c304a4e7535c86882b04aabf
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-04-07 20:31:09 -0700

    Fix hamburger menu not opening in navbar.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,22 +3,17 @@ import style from "./style.css";
 import { Link } from "react-router-dom";
 function Nav() {
   return (
-
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
         <Link className="nav-link" to="/">Google Books</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/search">Search</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/saved">Saved</Link>
-            </li>
-          </ul>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav ms-auto">
+            <Link className="nav-link" to="/search">Search</Link>
+            <Link className="nav-link" to="/saved">Saved</Link>
+          </div>
         </div>
       </div>
     </nav>
